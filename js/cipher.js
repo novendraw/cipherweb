d72c3c9edc573130ee8e9db6638a44cdb624c719
Eka Novendra Wahyunadi <ekanovendra726@gmail.com>
2020-09-06 16:08:37 +0700

    Add decryptVigenere

--- js/cipher.js
+++ js/cipher.js
@@ -19,3 +19,28 @@ function encryptVigenereCipher(text, key) {
 
     return resultText;
 }
+
+function decryptVigenereCipher(text, key) {
+    text = sanitizeText(text);
+    key = sanitizeText(key);
+    let textList = stringToIntList(text);
+    let keyList = stringToIntList(key);
+    let resultList = [];
+    let resultText = "";
+
+    for (let i = 0; i < textList.length; i++) {
+        resultList[i] = (textList[i] - keyList[i % keyList.length]) % 26;
+        while (resultList[i] < 0) {
+            resultList[i] += 26;
+        }
+    }
+
+    resultList = intsToCharList(resultList);
+
+    for (let i = 0; i < resultList.length; i++) {
+        resultText += resultList[i];
+
+    }
+
+    return resultText;
+}
