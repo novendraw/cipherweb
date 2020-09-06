1af97a8a95aaf560551c4de75d104be3bc3f9c11
Eka Novendra Wahyunadi <ekanovendra726@gmail.com>
2020-09-06 15:53:42 +0700

    Add encryptVigenere

---
+++ js/cipher.js
@@ -0,0 +1,21 @@
+function encryptVigenereCipher(text, key) {
+    text = sanitizeText(text);
+    key = sanitizeText(key);
+    let textList = stringToIntList(text);
+    let keyList = stringToIntList(key);
+    let resultList = [];
+    let resultText = "";
+
+    for (let i = 0; i < textList.length; i++) {
+        resultList[i] = (textList[i] + keyList[i % keyList.length]) % 26;
+
+    }
+    resultList = intsToCharList(resultList);
+
+    for (let i = 0; i < resultList.length; i++) {
+        resultText += resultList[i];
+
+    }
+
+    return resultText;
+}
