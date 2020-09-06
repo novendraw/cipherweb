e81bf93647ede52950b188e19dae10a39bb36a92
Eka Novendra Wahyunadi <ekanovendra726@gmail.com>
2020-09-06 15:54:06 +0700

    Remove index.js

--- js/index.js
+++
@@ -1,5 +0,0 @@
-const fileSelector = document.getElementById('css-selector');
-fileSelector.addEventListener('change', (event) => {
-    const fileList = event.target.files;
-    console.log(fileList);
-});
